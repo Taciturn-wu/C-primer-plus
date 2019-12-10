c2421c24002e36697b8eed83e826ff8a29dd480c
Taciturn-wu <3100563328@qq.com>
2019-12-11 00:13:24 +0800

    commit

--- cpp_6_10.c
+++ cpp_6_10.c
@@ -3,10 +3,11 @@
 int main()
 {
 	char a[8];
-	int i,len,j;
+	int i,len;
 	for(i=0;i<8;i++)
 	scanf("%d",&a[i]);
-	len=strlen(a);
-	for(i=len-1;i>=0;i--)
+	len=strlen(a)-1;
+	for(i=len;i>=0;i--)
 	printf("%d ",a[i]);
-}
+	printf("\n");
+} 
