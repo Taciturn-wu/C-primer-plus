c996533c61aff158e5274de797b3079acbf7273d
Taciturn-wu <3100563328@qq.com>
2019-12-11 19:54:42 +0800

    commit

--- cpp_6_10.c
+++ cpp_6_10.c
@@ -2,12 +2,13 @@
 #include <string.h>
 int main()
 {
-	char a[8];
-	int i,len;
-	for(i=0;i<8;i++)
-	scanf("%d",&a[i]);
-	len=strlen(a)-1;
-	for(i=len;i>=0;i--)
-	printf("%d ",a[i]);
+        char a[8];
+        int i,len;
+        for(i=0;i<8;i++)
+        scanf("%d",&a[i]);
+        len=strlen(a)-1;
+        for(i=len;i>=0;i--)
+        printf("%d ",a[i]);
 	printf("\n");
-} 
+}
+
